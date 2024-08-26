ea2ae5886fffccfb7caecf9990a572e93b5e60ef
MarcoLopez2020 <mlopez4183@uta.edu.ec>
2024-08-26 00:05:46 -0500

    Materiales y texturas

--- src/componentes/Scena.js
+++ src/componentes/Scena.js
@@ -7,10 +7,10 @@ const Scena = () => {
 
   useEffect(() => {
     const currentMount = mountRef.current;
-    
+
     // Crear la escena
     const scene = new THREE.Scene();
-    
+
     // Crear la cámara
     const camera = new THREE.PerspectiveCamera(
       25,
@@ -18,7 +18,7 @@ const Scena = () => {
       0.1,
       1000
     );
-    camera.position.z = 8; // Alejar la cámara para que se vea la escena
+    camera.position.z = 12; // Alejar la cámara para que se vea la escena
     scene.add(camera);
 
     // Crear el renderizador
@@ -28,31 +28,48 @@ const Scena = () => {
 
     //Control Orbital
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target = new THREE.Vector3(3, 3, 3)
-
+    //ubicacion del target
+   // controls.target = new THREE.Vector3(3, 0, 0)
+    controls.enableDamping = true;
     //Cubo Mallas *(geometria y material)(esqueleto y piel)
     const cube = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial()
+      new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        transparent: true,
+        opacity: 0.5,
+        wireframe : true
+      })
     )
     scene.add(cube)
     //Esfera
+    const textureloader = new THREE.TextureLoader()
+    const matcap1 = textureloader.load('./texturas/mapcap1.png');
+
+
     const geometry = new THREE.SphereGeometry(0.6, 32, 16);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    const material = new THREE.MeshMatcapMaterial({matcap: matcap1}
+      
+    );
     const sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
-    sphere.position.x = 3
-    sphere.position.y = 0.2
-    sphere.position.z = -3
+    sphere.position.x = 0
+    sphere.position.y = 1.5
+    sphere.position.z = 0
     //torus
-    const geometry1 = new THREE.TorusKnotGeometry(0.3, 0.02, 125, 16);
-    const material1 = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    const geometry1 = new THREE.TorusKnotGeometry(0.5, 0.18, 100, 10);
+    const material1 = new THREE.MeshNormalMaterial({
+      transparent: true,
+      opacity: 0.81,
+      flatShading: true
+
+    });
     const torusKnot = new THREE.Mesh(geometry1, material1);
     scene.add(torusKnot);
-    torusKnot.position.x = -2
+    //torusKnot.position.x = -2
     //torusKnot.scale.x   = 2
-    torusKnot.scale.set(2, 1, 1)
-
+    torusKnot.position.set(0, -1.5, 0)
+    torusKnot.scale.set(1, 1, 1)
     //Renderizar la escena
     //Frames para el renderer
     const animate = () => {
@@ -60,7 +77,7 @@ const Scena = () => {
       renderer.render(scene, camera);
       requestAnimationFrame(animate);
     }
-  animate()
+    animate()
 
     // Limpiar al desmontar el componente
     return () => {
