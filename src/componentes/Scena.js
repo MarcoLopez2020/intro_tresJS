ec82e8ee02954f8431a622c2b8f901994cca88e0
MarcoLopez2020 <mlopez4183@uta.edu.ec>
2024-08-27 23:46:03 -0500

    Luces y envmap con bg

--- src/componentes/Scena.js
+++ src/componentes/Scena.js
@@ -29,9 +29,79 @@ const Scena = () => {
     //Control Orbital
     const controls = new OrbitControls(camera, renderer.domElement);
     //ubicacion del target
-   // controls.target = new THREE.Vector3(3, 0, 0)
+    // controls.target = new THREE.Vector3(3, 0, 0)
     controls.enableDamping = true;
-    //Cubo Mallas *(geometria y material)(esqueleto y piel)
+    //Texturas
+    //Trae la imagen como una textura no como png o jpg
+    const textureloader = new THREE.TextureLoader()
+    const base = textureloader.load('./bricks/base.jpg')
+    const ao = textureloader.load('./bricks/ao.jpg')
+    const roug = textureloader.load('./bricks/roug.jpg')
+    const normal = textureloader.load('./bricks/normal.jpg')
+    const heigh = textureloader.load('./bricks/heigh.png')
+    const geometria = new THREE.BoxGeometry(1, 1, 1,
+      250,
+      250,
+      250
+    )
+    const material = new THREE.MeshStandardMaterial({
+      map: base,
+      aoMap: ao,
+      roughnessMap: roug,
+      normalMap: normal,
+      displacementMap: heigh,
+      displacementScale: 0.01
+    })
+    const cube1 = new THREE.Mesh(geometria, material)
+    scene.add(cube1)
+    cube1.position.set(1, 1, 1)
+    cube1.scale.set(3, 3, 3)
+    //luz necesaria para standard material para "verla"
+    const luz = new THREE.DirectionalLight(0xffffff, 1.4)
+
+    scene.add(luz)
+const enviromentMap = new THREE.CubeTextureLoader()
+const envMap = enviromentMap.load([
+  './invmap/nx.png', 
+  './invmap/px.png',
+  './invmap/ny.png',
+  './invmap/py.png', 
+  './invmap/nz.png',
+  './invmap/pz.png'
+])
+scene.environment = envMap
+scene.background = envMap
+
+luz.position.set(5,5,5)
+    const animate = () => {
+      controls.update(); // Actualizar los controles orbitales
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    }
+    animate()
+
+    // Limpiar al desmontar el componente
+    return () => {
+      currentMount.removeChild(renderer.domElement);
+      renderer.dispose();
+    };
+  }, []);
+
+
+  return (
+    <div
+      className="Contenedor 3D"
+      ref={mountRef}
+      style={{ width: '100%', height: '100vh' }}
+    ></div>
+  );
+};
+
+export default Scena;
+
+/**
+ mas geometrias
+     //Cubo Mallas *(geometria y material)(esqueleto y piel)
     const cube = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
       new THREE.MeshBasicMaterial({
@@ -72,28 +142,4 @@ const Scena = () => {
     torusKnot.scale.set(1, 1, 1)
     //Renderizar la escena
     //Frames para el renderer
-    const animate = () => {
-      controls.update(); // Actualizar los controles orbitales
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    }
-    animate()
-
-    // Limpiar al desmontar el componente
-    return () => {
-      currentMount.removeChild(renderer.domElement);
-      renderer.dispose();
-    };
-  }, []);
-
-
-  return (
-    <div
-      className="Contenedor 3D"
-      ref={mountRef}
-      style={{ width: '100%', height: '100vh' }}
-    ></div>
-  );
-};
-
-export default Scena;
+     */
